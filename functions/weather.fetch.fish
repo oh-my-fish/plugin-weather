bf0356d2574bdc50a809246ec444b21abbbf1ab0
Stephen Coakley <me@stephencoakley.com>
2015-11-28 15:49:05 -0600

    Add GET parameters and more md5 providers

--- functions/weather.fetch.fish
+++ functions/weather.fetch.fish
@@ -1,10 +1,35 @@
 function weather.fetch -d "Fetches data from a URL backed by a cache"
-  set md5 (echo $argv[1] | md5sum | cut -d " " -f 1)
+  set md5 (__md5 "$argv")
 
-  if not find /tmp/$md5.url -mmin +30 ^ /dev/null
-    curl -s $argv[1] > /tmp/$md5.url
+  if test (count $argv) -gt 1
+    for param in $argv[2..-1]
+      set flags $flags --data-urlencode $param
+    end
+  end
+
+  if not find /tmp/$md5.url -mmin +$weather_cache_age > /dev/null ^ /dev/null
+    curl -Gs $flags $argv[1] > /tmp/$md5.url
       or return 1
   end
 
   cat /tmp/$md5.url
+end
+
+function __md5
+  if available md5sum
+    echo $argv[1] | md5sum | cut -d " " -f 1
+    return 0
+  end
+
+  if available md5
+    echo $argv[1] | md5 | cut -d " " -f 2
+    return 0
+  end
+
+  if available openssl
+    echo $argv[1] | openssl md5 | cut -d " " -f 2
+    return 0
+  end
+
+  return 1
 end
