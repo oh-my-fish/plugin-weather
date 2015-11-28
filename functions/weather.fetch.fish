cf4652f5a3e29c88554adf28f97247cd980d299a
Stephen Coakley <me@stephencoakley.com>
2015-11-28 12:30:51 -0600

    Fix cache search

--- functions/weather.fetch.fish
+++ functions/weather.fetch.fish
@@ -1,7 +1,7 @@
 function weather.fetch -d "Fetches data from a URL backed by a cache"
   set md5 (echo $argv[1] | md5sum | cut -d " " -f 1)
 
-  if not find /tmp/$md5.url -mmin +30 2> /dev/null
+  if not find /tmp/$md5.url -mmin +30 ^ /dev/null
     curl -s $argv[1] > /tmp/$md5.url
       or return 1
   end
