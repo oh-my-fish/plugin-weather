e5cac28c8c665d9d8b960fe8642bd81376adb21b
Stephen Coakley <me@stephencoakley.com>
2016-01-01 19:55:11 -0500

    Add note about requiring jq >=1.5 before running

--- functions/weather.fish
+++ functions/weather.fish
@@ -4,6 +4,10 @@ function weather -d "Displays weather info"
     echo "The jq program is required to parse weather data."
     echo "See https://stedolan.github.io/jq for details."
     return 1
+  else if math (jq --version | cut -d - -f 2)"<1.5" > /dev/null
+    echo "You must have jq version 1.5 or newer installed to parse weather data."
+    echo "You can download the latest version of jq from https://stedolan.github.io/jq."
+    return 1
   end
 
   # Display help message.
