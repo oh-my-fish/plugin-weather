30908d84dc9de98a5035f742e55e07cca7f20e59
twister <twister.kz@gmail.com>
2019-02-13 19:21:29 +0600

    New geoip API support

--- functions/weather.location.fish
+++ functions/weather.location.fish
@@ -6,16 +6,16 @@ function weather.location -d "Get the current geographic location"
   end
 
   # Fetch location data based on our IP
-  if not set geoip_data (weather.fetch "http://geoip.nekudo.com/api/$ip")
+  if not set geoip_data (weather.fetch "https://ipapi.com/ip_api.php?ip=$ip")
     echo "Unable to query GeoIP data; please try again later."
     return 1
   end
 
   # Echo coordiantes.
-  echo $geoip_data | jq '.location.latitude'
-  echo $geoip_data | jq '.location.longitude'
+  echo $geoip_data | jq '.latitude'
+  echo $geoip_data | jq '.longitude'
   echo $geoip_data | jq -r '.city?'
-  echo $geoip_data | jq -r '.country.name'
+  echo $geoip_data | jq -r '.country_name'
 end
 
 function __weather_get_ip -d "Get the current device's public IP address"
