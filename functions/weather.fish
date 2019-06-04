8e2ce677c47afd6571b27d44599aaacc507d18fe
Lukas Alberto Belck <lukas.belck@gmail.com>
2019-06-04 14:44:22 -0300

    Fix math comparison
    The new math builtin in 3.0 does not support comparisons. test can now do floats, though.

--- functions/weather.fish
+++ functions/weather.fish
@@ -8,7 +8,7 @@ function weather -d "Displays weather info"
     return 1
   else
     set -l jq_version (jq --version 2>&1 | tr -dC '[:digit:].')
-    if math "$jq_version<1.5" > /dev/null
+    if test "$jq_version" -gt "1.5" > /dev/null
       echo "jq version $jq_version detected"
       echo "You must have jq version 1.5 or newer installed to parse weather data."
       echo "You can download the latest version of jq from https://stedolan.github.io/jq."
